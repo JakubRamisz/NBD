f7f04c2d32c2130928f38826e4b5ccedebab58f5
Jakub Ramisz <jakubramisz01@gmail.com>
2022-10-12 09:44:28 +0200

    Add base class

---
+++ workshop/base.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('postgresql://nbd:nbdpassword@127.0.0.1:5432/nbddb')
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
