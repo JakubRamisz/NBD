3e0a41d1324f32aae08a0aeb1b8819283a90fc5d
Jakub Ramisz <jakubramisz01@gmail.com>
2022-10-14 13:38:14 +0200

    Change project structure

--- bank/models/client.py
+++ bank/models/client.py
@@ -1,6 +1,6 @@
-import string
 from sqlalchemy import Column, Integer, String
-from base import Base
+from sqlalchemy.orm import relationship
+from models.base import Base
 
 class Client(Base):
     __tablename__ = 'clients'
@@ -8,7 +8,8 @@ class Client(Base):
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
+    # accounts = relationship("Account")
 
-    def __init__(self, first_name: string, last_name: string) -> None:
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
