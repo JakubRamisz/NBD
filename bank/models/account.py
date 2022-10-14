1d4d9f1590dcf82df09ad62e6f49cb9ed681db83
Jakub Ramisz <jakubramisz01@gmail.com>
2022-10-14 20:27:19 +0200

    Add account type models

--- bank/models/account.py
+++ bank/models/account.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, Numeric, String, ForeignKey
+from datetime import datetime
+from sqlalchemy import Column, Integer, Numeric, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from models.base import Base
 
@@ -11,8 +12,36 @@ class Account(Base):
     owner_id = Column(Integer, ForeignKey('clients.id'))
     owner = relationship('Client', backref='account')
 
+    type = Column(String)
 
-    def __init__(self, account_number, owner, balance=0):
+    __mapper_args__ = {
+        'polymorphic_on': 'type',
+        'polymorphic_identity': 'account',
+    }
+
+
+    def __init__(self, account_number, owner, balance):
         self.account_number = account_number
         self.owner = owner
         self.balance = balance
+
+
+class PersonalAccount(Account):
+    __mapper_args__ = {
+        'polymorphic_identity': 'personal_account',
+    }
+
+
+class SavingsAccount(Account):
+    rate = Column(Numeric)
+    last_update_date = Column(DateTime, nullable=True)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'savings_account',
+    }
+
+    def __init__(self, account_number, owner, balance, rate):
+        super().__init__(account_number, owner, balance)
+
+        self.rate = rate
+        self.last_update_date = self.date = datetime.now()
--- bank/models/client.py
+++ bank/models/client.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from models.base import Base
 
 class Client(Base):
