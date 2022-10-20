38824ad7660e20e7ca1bd0fb24384010c2095a3b
Jakub Ramisz <jakubramisz01@gmail.com>
2022-10-20 13:18:19 +0200

    Add main function

--- inserts.py
+++ inserts.py
@@ -3,13 +3,19 @@ from managers.account_management import add_personal_account, add_savings_accoun
 from managers.transaction_management import deposit, transfer
 from managers.client_management import add_client
 
-Base.metadata.create_all(engine)
 
-client_id = add_client('Jan', 'Kowalski')
-acc_id1 = add_personal_account('123', client_id)
-acc_id2 = add_savings_account('456', client_id, 0.1)
+def main():
+    Base.metadata.create_all(engine)
+
+    client_id = add_client('Jan', 'Kowalski')
+    acc_id1 = add_personal_account('123', client_id)
+    acc_id2 = add_savings_account('456', client_id, 0.1)
 
 
-deposit(acc_id1, 200)
+    deposit(acc_id1, 200)
 
-transfer(acc_id1, acc_id2, 50)
+    transfer(acc_id1, acc_id2, 50)
+
+
+if __name__ == '__main__':
+    main()
